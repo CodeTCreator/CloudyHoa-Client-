aba4830b1726038bd9c3bf440cd64489bec6cbbd
Богдан Т <bogdantribusyan2016@gmail.com>
2024-02-17 21:19:54 +0300

    Окно объектов и модальное окно для добавления и редактирования объектов.

--- CloudyHoa(Client)/General/IFocusedObject.cs
+++ CloudyHoa(Client)/General/IFocusedObject.cs
@@ -8,12 +8,6 @@ namespace CloudyHoa_Client_.General
 {
     internal interface IFocusedObject
     {
-<<<<<<<< HEAD:CloudyHoa(Client)/General/IFocusedObject.cs
         FocusedObject focusedObject { get; set; }
-========
-        void LoadData(string name, string type);
-
-        string GetData();
->>>>>>>> eb43c81 (Окно объектов и модальное окно для добавления и редактирования объектов.):CloudyHoa(Client)/ObjectWindows/MW/ISPControl.cs
     }
 }
