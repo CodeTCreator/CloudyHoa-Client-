1b5bba1961c9cf0eedede3c4b46a80b9e0a0ebb6
Богдан Т <bogdantribusyan2016@gmail.com>
2024-02-17 21:19:54 +0300

    Окно объектов и модальное окно для добавления и редактирования объектов.

---
+++ CloudyHoa(Client)/ObjectWindows/MW/ISPControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudyHoa_Client_.ObjectWindows.MW
+{
+    internal interface ISPControl
+    {
+        void LoadData(string name, string type);
+
+        string GetData();
+    }
+}
