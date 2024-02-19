5ac1767e81692bd75741f5f7198270d58022958d
Богдан Т <bogdantribusyan2016@gmail.com>
2024-02-19 13:19:42 +0300

    Создание окна. Создание контроллеров.

---
+++ CloudyHoa(Client)/PropertiesWindows/PropertiesWindow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CloudyHoa_Client_.PropertiesWindows
+{
+    public partial class PropertiesWindow : Form
+    {
+        public PropertiesWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
