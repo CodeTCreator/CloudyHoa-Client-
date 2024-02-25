b7cc1ce9aeabae6ecdcd7fac2de619110fcd79b8
Богдан Т <bogdantribusyan2016@gmail.com>
2024-02-26 00:20:43 +0300

    Добавление UserControl для окна параметров объекта.

---
+++ CloudyHoa(Client)/PropertiesWindows/MW/UserControls/SPControl(Date).Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace CloudyHoa_Client_.PropertiesWindows.MW.UserControls
+{
+    partial class SPControl_Date
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.valueControl = new DevExpress.XtraEditors.DateEdit();
+            this.textEditDataType = new DevExpress.XtraEditors.TextEdit();
+            this.textEditName = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.valueControl.Properties.CalendarTimeProperties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valueControl.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEditDataType.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEditName.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.valueControl);
+            this.panelControl1.Controls.Add(this.textEditDataType);
+            this.panelControl1.Controls.Add(this.textEditName);
+            this.panelControl1.Controls.Add(this.labelControl3);
+            this.panelControl1.Controls.Add(this.labelControl2);
+            this.panelControl1.Controls.Add(this.labelControl1);
+            this.panelControl1.Location = new System.Drawing.Point(0, 0);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(460, 185);
+            this.panelControl1.TabIndex = 0;
+            // 
+            // valueControl
+            // 
+            this.valueControl.EditValue = null;
+            this.valueControl.Location = new System.Drawing.Point(225, 137);
+            this.valueControl.Name = "valueControl";
+            this.valueControl.Properties.Appearance.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.valueControl.Properties.Appearance.Options.UseFont = true;
+            this.valueControl.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.Simple;
+            this.valueControl.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.valueControl.Properties.CalendarTimeProperties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.valueControl.Size = new System.Drawing.Size(210, 30);
+            this.valueControl.TabIndex = 17;
+            // 
+            // textEditDataType
+            // 
+            this.textEditDataType.Enabled = false;
+            this.textEditDataType.Location = new System.Drawing.Point(225, 78);
+            this.textEditDataType.Name = "textEditDataType";
+            this.textEditDataType.Properties.Appearance.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textEditDataType.Properties.Appearance.Options.UseFont = true;
+            this.textEditDataType.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.Simple;
+            this.textEditDataType.Size = new System.Drawing.Size(210, 30);
+            this.textEditDataType.TabIndex = 16;
+            // 
+            // textEditName
+            // 
+            this.textEditName.Enabled = false;
+            this.textEditName.Location = new System.Drawing.Point(225, 19);
+            this.textEditName.Name = "textEditName";
+            this.textEditName.Properties.Appearance.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textEditName.Properties.Appearance.Options.UseFont = true;
+            this.textEditName.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.Simple;
+            this.textEditName.Size = new System.Drawing.Size(210, 30);
+            this.textEditName.TabIndex = 15;
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Appearance.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelControl3.Appearance.Options.UseFont = true;
+            this.labelControl3.Location = new System.Drawing.Point(37, 132);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(100, 31);
+            this.labelControl3.TabIndex = 14;
+            this.labelControl3.Text = "Значение";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.labelControl2.Location = new System.Drawing.Point(37, 74);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(122, 31);
+            this.labelControl2.TabIndex = 13;
+            this.labelControl2.Text = "Тип данных";
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Location = new System.Drawing.Point(37, 18);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(100, 31);
+            this.labelControl1.TabIndex = 12;
+            this.labelControl1.Text = "Название";
+            // 
+            // SPControl_Date
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panelControl1);
+            this.Name = "SPControl_Date";
+            this.Size = new System.Drawing.Size(460, 185);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            this.panelControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.valueControl.Properties.CalendarTimeProperties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valueControl.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEditDataType.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEditName.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.DateEdit valueControl;
+        private DevExpress.XtraEditors.TextEdit textEditDataType;
+        private DevExpress.XtraEditors.TextEdit textEditName;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+    }
+}
