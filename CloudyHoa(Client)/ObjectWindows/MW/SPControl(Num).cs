2eab9c0301a5365255572ae68168cb95838d32ee
Богдан Т <bogdantribusyan2016@gmail.com>
2024-03-14 11:41:39 +0300

    Новый UserControl для ввода числового параметра.

---
+++ CloudyHoa(Client)/ObjectWindows/MW/SPControl(Num).cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CloudyHoa_Client_.ObjectWindows.MW
+{
+    public partial class SPControl_Num : UserControl, ISPControl
+    {
+        public SPControl_Num()
+        {
+            InitializeComponent();
+        }
+
+        public string GetData()
+        {
+           return valueControl.Value.ToString();
+        }
+
+        public void LoadData(string name, string type)
+        {
+            textEditName.Text = name;
+            textEditDataType.Text = type;
+        }
+    }
+}
