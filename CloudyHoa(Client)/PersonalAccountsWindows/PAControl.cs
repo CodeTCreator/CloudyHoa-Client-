ec01d48c42d926dd64d4b6274acf203d720aa154
Богдан Т <bogdantribusyan2016@gmail.com>
2024-04-12 17:16:54 +0300

    Обновление проекта.

---
+++ CloudyHoa(Client)/PersonalAccountsWindows/PAControl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CloudyHoa_Client_.PersonalAccountsWindows
+{
+    public partial class PAControl : UserControl
+    {
+        public PAControl()
+        {
+            InitializeComponent();
+        }
+
+        private void addTenantButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PAControl_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
