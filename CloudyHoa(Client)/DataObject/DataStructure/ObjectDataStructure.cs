740ffeb781b3c9f335b8fb59646afee35ebc79a2
Богдан Т <bogdantribusyan2016@gmail.com>
2024-04-27 19:32:17 +0300

    Добавление поля "Выбранный объект" для уменьшения дублирования кода.

--- CloudyHoa(Client)/DataObject/DataStructure/ObjectDataStructure.cs
+++ CloudyHoa(Client)/DataObject/DataStructure/ObjectDataStructure.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CloudyHoa_Client_.General;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CloudyHoa_Client_.DataObject.DataStructure
 {
     internal class ObjectDataStructure : IDataStructure
     {
+        FocusedObject _focusedObject = new FocusedObject();
         DataTable _dataObjectTable = new DataTable();
         
         public ObjectDataStructure() { }
 
+        public FocusedObject FocusedObject { get { return _focusedObject; } set { _focusedObject = value; } }
         public DataTable DataTable { get { return _dataObjectTable; } set { _dataObjectTable = value; } }
     }
 }
