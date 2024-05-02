be92fbb2bae209edc6dcd98e4f20ad497cf36e51
Богдан Т <bogdantribusyan2016@gmail.com>
2024-05-02 12:02:33 +0300

    Контроллеры для основного окна л/с.

---
+++ CloudyHoa(Client)/PersonalAccountsWindows/Controllers/PAWindowController.cs
@@ -0,0 +1,38 @@
+﻿using CloudyHoa_Client_.DataObject.DataStructure;
+using CloudyHoa_Client_.General;
+using CloudyHoa_Client_.ObjectWindow.Service_Controller;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace CloudyHoa_Client_.PersonalAccountsWindows.Controllers
+{
+    internal class PAWindowController
+    {
+        public PAWindowController() { }
+
+
+        public async Task LoadPAsTableAsync(PADataService pADataService, PADataStructure pADataStructure, FocusedObject focusedObject)
+        {
+            DataTable dataTable = await pADataService.GetPAs(focusedObject.objectId);
+            dataTable.TableName = "Residents";
+            pADataStructure.PersonalAccountTable = dataTable;
+        }
+
+        public DataTable GetPATable(PADataStructure pADataStructure)
+        {
+            return pADataStructure.PersonalAccountTable;
+        }
+
+        public async Task LoadObjectsTableAsync(ObjectDataService objectDataService, ObjectDataStructure objectDataStructure)
+        {
+            DataTable dataTable = await objectDataService.GetAllObjectsAsync(UserContext.Instance.CurrentUser.hoaId);
+            dataTable.TableName = "Objects";
+            objectDataStructure.DataTable = dataTable;
+        }
+
+        public FocusedPA GetFocusedPA(PADataStructure pADataStructure)
+        {
+            return pADataStructure.FocusedPA;
+        }
+    }
+}
