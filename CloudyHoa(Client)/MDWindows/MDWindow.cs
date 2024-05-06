71c2d443864363a2526031ace55ac43431babe3b
Богдан Т <bogdantribusyan2016@gmail.com>
2024-05-06 23:24:14 +0300

    Основное окно для приборов учета

--- CloudyHoa(Client)/MDWindows/MDWindow.cs
+++ CloudyHoa(Client)/MDWindows/MDWindow.cs
@@ -2,7 +2,10 @@
 using CloudyHoa_Client_.DataObject.DataStructure;
 using CloudyHoa_Client_.General;
 using CloudyHoa_Client_.MDWindows.Controllers;
+using CloudyHoa_Client_.MDWindows.MW;
+using CloudyHoa_Client_.MetadataService;
 using CloudyHoa_Client_.ObjectWindow.Service_Controller;
+using CloudyHoa_Client_.PersonalAccountsWindows.Controllers;
 using CloudyHoa_Client_.SaveExecuteControl;
 using System;
 using System.Collections.Generic;
@@ -18,14 +21,19 @@ namespace CloudyHoa_Client_.MDWindows
 {
     public partial class MDWindow : Form, ISafeExecuteControl
     {
+        PADataStructure _paDataStructure;
+        PADataService _paDataService;
         ObjectDataStructure _objectDataStructure;
         ObjectDataService _objectDataService;
         MDDataStructure _structure;
         MDWindowController _controller;
+        MDDataService _service;
+        PAWindowController _paWindowController;
 
-        public Control ContainerForLoading => throw new NotImplementedException();
 
-        public Control LockControl => throw new NotImplementedException();
+        public Control ContainerForLoading => panelControlMain;
+
+        public Control LockControl => this;
 
         public MDWindow()
         {
@@ -44,41 +52,152 @@ namespace CloudyHoa_Client_.MDWindows
         }
         private void BindingPATable()
         {
-            gridControlPA.DataSource = _controller.GetPATable(_structure);
+            gridControlPA.DataSource = _paWindowController.GetPATable(_paDataStructure);
         }
         private void BindingMDTable()
         {
-            gridControlMW.DataSource = _controller.GetMDTable(_structure);
+            gridControlMD.DataSource = _controller.GetMDTable(_structure);
         }
 
-        private void LoadData()
+        private async Task LoadData()
         {
-            LoadObjectsTable();
-            LoadPATable();
-            LoadMDTable();
+            await LoadObjectsTable();
         }
-        private void LoadObjectsTable()
+        private async Task LoadObjectsTable()
         {
-            GeneralLoadData.LoadData(this,_controller.LoadObjectsTableAsync, _objectDataStructure, _objectDataService);
+           await GeneralLoadData.LoadData(this,_controller.LoadObjectsTableAsync, _objectDataStructure, _objectDataService);
         }
-        private void LoadPATable()
+        private async Task LoadPATable()
         {
+            await this.SafeUIExecuteAsync(async () =>
+            {
+                try
+                {
+                    await _paWindowController.LoadPAsTableAsync(_paDataService, _paDataStructure, _objectDataStructure.FocusedObject);
 
+                }
+                catch (Exception e)
+                {
+                    const string caption = "Подключение к серверу";
+                    var result = MessageBox.Show(e.Message, caption,
+                                                 MessageBoxButtons.OK,
+                                                 MessageBoxIcon.Error);
+                }
+            }, "Ошибка подключения к северу.");
         }
-        private void LoadMDTable()
+        private async Task LoadMDTableO()
         {
-
+           await GeneralLoadData.LoadData(this, _controller.LoadMDsTableAsyncO, _structure, _service, _objectDataStructure.FocusedObject.objectId);
+        }
+        private async Task LoadMDTableP()
+        {
+           await GeneralLoadData.LoadData(this, _controller.LoadMDsTableAsyncP, _structure, _service, _paDataStructure.FocusedPA.paId);
         }
 
-        private void MDWindow_Load(object sender, EventArgs e)
+        private async void MDWindow_Load(object sender, EventArgs e)
         {
             _controller = new MDWindowController();
             _structure = new MDDataStructure();
             _objectDataStructure = new ObjectDataStructure();
-            LoadData();
+            _objectDataService = new ObjectDataService();
+            _paDataStructure = new PADataStructure();
+            _paDataService = new PADataService();
+            _paWindowController = new PAWindowController();
+            _service = new MDDataService();
 
+
+            treeListControl1.TreeClick += objectControl_Click;
+            await LoadData();
 
             BindingData();
+        }
+        private async void objectControl_Click()
+        {
+            FocusedObject focusedObject = treeListControl1.GetFocusedObject();
+            _objectDataStructure.FocusedObject = focusedObject;
+
+            await LoadPATable();
+            BindingPATable();
+            await LoadMDTableO();
+            BindingMDTable();
+
+            addMDButton.Enabled = false;
+            editMDButton.Enabled = false;
+            deleteMDButton.Enabled = false;
+        }
+
+        private async void gridControlPA_Click(object sender, EventArgs e)
+        {
+            if (gridViewPA.GetFocusedRowCellValue("id") != null)
+            {
+                _paDataStructure.FocusedPA.paId = (int)gridViewPA.GetFocusedRowCellValue("id");
+                _paDataStructure.FocusedPA.resposibleId = (int)gridViewPA.GetFocusedRowCellValue("owner_id");
+                _paDataStructure.FocusedPA.Account = gridViewPA.GetFocusedRowCellValue("account").ToString();
+
+                _paDataStructure.FocusedPA.Registered = (int)gridViewPA.GetFocusedRowCellValue("registered");
+                _paDataStructure.FocusedPA.Lives = (int)gridViewPA.GetFocusedRowCellValue("lives");
+                _paDataStructure.FocusedPA.objectId = (int)gridViewPA.GetFocusedRowCellValue("object_id");
+
+                await LoadMDTableP();
+                BindingMDTable();
+
+                addMDButton.Enabled = true;
+                editMDButton.Enabled = false;
+                deleteMDButton.Enabled = false;
+            }
+        }
+
+        private async void deleteMDButton_Click(object sender, EventArgs e)
+        {
+            if (gridViewMD.GetFocusedRowCellValue("id") != null)
+            {
+                int id = (int)gridViewMD.GetFocusedRowCellValue("id");
+                _service.DeleteMeteringDevice(id);
+                await LoadMDTableO();
+                BindingMDTable();
+            }
+        }
+
+        private void gridControlMD_Click(object sender, EventArgs e)
+        {
+            if (gridViewMD.GetFocusedRowCellValue("id") != null)
+            {
+                _structure.FocusedMD.meteringDeviceId = (int)gridViewMD.GetFocusedRowCellValue("id");
+                _structure.FocusedMD.number = gridViewMD.GetFocusedRowCellValue("number").ToString();
+                _structure.FocusedMD.verificationDate = DateTime.Parse(gridViewMD.GetFocusedRowCellValue("verification_date").ToString());
+                _structure.FocusedMD.installationDate = DateTime.Parse(gridViewMD.GetFocusedRowCellValue("installation_date").ToString());
+                _structure.FocusedMD.serviceId = (int)gridViewMD.GetFocusedRowCellValue("service_id");
+                _structure.FocusedMD.personalAccountId = (int)gridViewMD.GetFocusedRowCellValue("personal_account_id");
+                GetPANumber();
+
+                editMDButton.Enabled = true;
+                deleteMDButton.Enabled = true;
+            }
+        }
+        private void GetPANumber()
+        {
+            _paDataStructure.FocusedPA.Account = 
+                _paDataStructure.PersonalAccountTable.Select("id = " + _structure.FocusedMD.personalAccountId).First()["account"].ToString();
+        }
+        private async void addMDButton_Click(object sender, EventArgs e)
+        {
+            MDMW mDMW = new MDMW(_objectDataStructure.FocusedObject.typeObject,
+                _objectDataStructure.FocusedObject.nameType + " " + _objectDataStructure.FocusedObject.identificator,
+                _paDataStructure.FocusedPA.Account, _paDataStructure.FocusedPA.paId, _service);
+            mDMW.ShowDialog();
+            await LoadMDTableO();
+            BindingMDTable();
+        }
+
+        private async void editMDButton_Click(object sender, EventArgs e)
+        {
+            
+            MDMW mDMW = new MDMW(_objectDataStructure.FocusedObject.typeObject,
+                _objectDataStructure.FocusedObject.nameType + " " + _objectDataStructure.FocusedObject.identificator,
+                _paDataStructure.FocusedPA.Account, _structure.FocusedMD, _service);
+            mDMW.ShowDialog();
+            await LoadMDTableO();
+            BindingMDTable();
         }
     }
 }
