67724b039b9cf789e6c55a26dff18f1f94a30f40
Богдан Т <bogdantribusyan2016@gmail.com>
2024-05-14 13:31:38 +0300

    Модификация универсального вызова методов загрузки данных

---
+++ CloudyHoa(Client)/General/IDataService.cs
@@ -0,0 +1,14 @@
+﻿using CloudyHoa_Client_.DataObject.DataStructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudyHoa_Client_.General
+{
+    public interface IDataService
+    {
+       
+    }
+}
