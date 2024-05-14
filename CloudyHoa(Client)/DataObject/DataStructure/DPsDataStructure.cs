cf571f3a5c0706681e988b07afb06d4137749e42
Богдан Т <bogdantribusyan2016@gmail.com>
2024-05-14 13:28:36 +0300

    Структура с таблицей динамических параметров

---
+++ CloudyHoa(Client)/DataObject/DataStructure/DPsDataStructure.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudyHoa_Client_.DataObject.DataStructure
+{
+    internal class DPsDataStructure: IDataStructure
+    {
+        public DataTable OldDynamicParams { get; set; } = new DataTable();
+    }
+}
