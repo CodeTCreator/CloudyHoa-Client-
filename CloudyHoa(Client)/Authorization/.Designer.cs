d7aec88dcf22cd62cd349e31a90d9ca1bea45566
Богдан Т <bogdantribusyan2016@gmail.com>
2024-05-16 12:35:07 +0300

    Обновление дизайна

--- CloudyHoa(Client)/Authorization/.Designer.cs
+++ CloudyHoa(Client)/Authorization/.Designer.cs
@@ -30,13 +30,13 @@
         {
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
             this.panelControl2 = new DevExpress.XtraEditors.PanelControl();
+            this.rememberCheckBox = new DevExpress.XtraEditors.CheckEdit();
             this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
             this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
             this.loginButton = new DevExpress.XtraEditors.SimpleButton();
-            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
-            this.rememberCheckBox = new DevExpress.XtraEditors.CheckEdit();
             this.passwordEdit = new DevExpress.XtraEditors.TextEdit();
             this.loginEdit = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
             this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
             ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).BeginInit();
             this.panelControl2.SuspendLayout();
@@ -51,7 +51,7 @@
             this.labelControl1.Appearance.Font = new System.Drawing.Font("Segoe UI Semibold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelControl1.Appearance.Options.UseFont = true;
             this.labelControl1.Location = new System.Drawing.Point(321, 34);
-            this.labelControl1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.labelControl1.Margin = new System.Windows.Forms.Padding(5);
             this.labelControl1.Name = "labelControl1";
             this.labelControl1.Size = new System.Drawing.Size(285, 54);
             this.labelControl1.TabIndex = 0;
@@ -70,17 +70,26 @@
             this.panelControl2.Controls.Add(this.loginEdit);
             this.panelControl2.Controls.Add(this.labelControl3);
             this.panelControl2.Location = new System.Drawing.Point(541, 128);
-            this.panelControl2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.panelControl2.Margin = new System.Windows.Forms.Padding(5);
             this.panelControl2.Name = "panelControl2";
             this.panelControl2.Size = new System.Drawing.Size(400, 482);
             this.panelControl2.TabIndex = 3;
+            // 
+            // rememberCheckBox
+            // 
+            this.rememberCheckBox.Location = new System.Drawing.Point(88, 323);
+            this.rememberCheckBox.Margin = new System.Windows.Forms.Padding(5);
+            this.rememberCheckBox.Name = "rememberCheckBox";
+            this.rememberCheckBox.Properties.Caption = "Запомнить";
+            this.rememberCheckBox.Size = new System.Drawing.Size(148, 24);
+            this.rememberCheckBox.TabIndex = 18;
             // 
             // labelControl4
             // 
             this.labelControl4.Appearance.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelControl4.Appearance.Options.UseFont = true;
             this.labelControl4.Location = new System.Drawing.Point(178, 218);
-            this.labelControl4.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.labelControl4.Margin = new System.Windows.Forms.Padding(5);
             this.labelControl4.Name = "labelControl4";
             this.labelControl4.Size = new System.Drawing.Size(63, 23);
             this.labelControl4.TabIndex = 16;
@@ -91,7 +100,7 @@
             this.labelControl2.Appearance.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelControl2.Appearance.Options.UseFont = true;
             this.labelControl2.Location = new System.Drawing.Point(178, 113);
-            this.labelControl2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.labelControl2.Margin = new System.Windows.Forms.Padding(5);
             this.labelControl2.Name = "labelControl2";
             this.labelControl2.Size = new System.Drawing.Size(52, 23);
             this.labelControl2.TabIndex = 15;
@@ -100,32 +109,12 @@
             // loginButton
             // 
             this.loginButton.Location = new System.Drawing.Point(163, 412);
-            this.loginButton.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.loginButton.Margin = new System.Windows.Forms.Padding(5);
             this.loginButton.Name = "loginButton";
             this.loginButton.Size = new System.Drawing.Size(100, 40);
             this.loginButton.TabIndex = 14;
             this.loginButton.Text = "Войти";
             this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
-            // 
-            // labelControl3
-            // 
-            this.labelControl3.Appearance.Font = new System.Drawing.Font("Segoe UI Semibold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelControl3.Appearance.Options.UseFont = true;
-            this.labelControl3.Location = new System.Drawing.Point(115, 24);
-            this.labelControl3.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
-            this.labelControl3.Name = "labelControl3";
-            this.labelControl3.Size = new System.Drawing.Size(166, 37);
-            this.labelControl3.TabIndex = 11;
-            this.labelControl3.Text = "Авторизация";
-            // 
-            // rememberCheckBox
-            // 
-            this.rememberCheckBox.Location = new System.Drawing.Point(88, 323);
-            this.rememberCheckBox.Margin = new System.Windows.Forms.Padding(5);
-            this.rememberCheckBox.Name = "rememberCheckBox";
-            this.rememberCheckBox.Properties.Caption = "Запомнить";
-            this.rememberCheckBox.Size = new System.Drawing.Size(148, 24);
-            this.rememberCheckBox.TabIndex = 18;
             // 
             // passwordEdit
             // 
@@ -149,11 +138,22 @@
             this.loginEdit.Size = new System.Drawing.Size(224, 26);
             this.loginEdit.TabIndex = 12;
             // 
+            // labelControl3
+            // 
+            this.labelControl3.Appearance.Font = new System.Drawing.Font("Segoe UI Semibold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelControl3.Appearance.Options.UseFont = true;
+            this.labelControl3.Location = new System.Drawing.Point(115, 24);
+            this.labelControl3.Margin = new System.Windows.Forms.Padding(5);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(166, 37);
+            this.labelControl3.TabIndex = 11;
+            this.labelControl3.Text = "Авторизация";
+            // 
             // panelControl1
             // 
             this.panelControl1.ContentImage = global::CloudyHoa_Client_.Properties.Resources.orig1;
             this.panelControl1.Location = new System.Drawing.Point(15, 128);
-            this.panelControl1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.panelControl1.Margin = new System.Windows.Forms.Padding(6);
             this.panelControl1.Name = "panelControl1";
             this.panelControl1.Size = new System.Drawing.Size(515, 482);
             this.panelControl1.TabIndex = 2;
@@ -168,6 +168,7 @@
             this.Controls.Add(this.labelControl1);
             this.Name = "LoginForm";
             this.Text = "Авторизация";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.LoginForm_FormClosing);
             this.Load += new System.EventHandler(this.LoginForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).EndInit();
             this.panelControl2.ResumeLayout(false);
