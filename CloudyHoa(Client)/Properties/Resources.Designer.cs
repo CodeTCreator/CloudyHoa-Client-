688fef1d5bbeef1aeacad9a3ec531e260a944d87
Богдан Т <bogdantribusyan2016@gmail.com>
2024-05-18 20:53:28 +0300

    Остальная конфигурация проекта и сервисов

--- CloudyHoa(Client)/Properties/Resources.Designer.cs
+++ CloudyHoa(Client)/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace CloudyHoa_Client_.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap saveall_32x32 {
+            get {
+                object obj = ResourceManager.GetObject("saveall_32x32", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
