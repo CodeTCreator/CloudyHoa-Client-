57a5f48cec6a9962cda2dae186dacfbd0ac7f5d3
Богдан Т <bogdantribusyan2016@gmail.com>
2024-07-15 20:52:32 +0300

    Интеграция универсального метода

---
+++ CloudyHoa(Client)/CalculationWindows/Controllers/CalculationWindowController.cs
@@ -0,0 +1,29 @@
+﻿using CloudyHoa_Client_.DataObject;
+using CloudyHoa_Client_.DataObject.DataStructure;
+using CloudyHoa_Client_.ObjectWindow.Service_Controller;
+using DevExpress.Utils.Design;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudyHoa_Client_.CalculationWindows.Controllers
+{
+    public class CalculationWindowController
+    {
+
+        public CalculationWindowController() { }
+        public async Task LoadObjectsTable(IDataService dataService, IDataStructure dataStructure)
+        {
+            DataTable dataTable = dataService.GetType() == typeof(ObjectDataStructure) ?
+                await ((ObjectDataService)dataService).GetCategories() : null;
+            dataTable.TableName = "Objects";
+            if (dataStructure.GetType() == typeof(ObjectDataStructure))
+            {
+                ((ObjectDataStructure)dataStructure).DataTable = dataTable;
+            }
+        }
+    }
+}
