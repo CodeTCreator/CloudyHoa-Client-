3ad8984772ddc68dc504f2708b03131b5af2061d
Богдан Т <bogdantribusyan2016@gmail.com>
2024-08-25 16:23:40 +0300

    Создание структуры данных для окна "История показаний"

---
+++ CloudyHoa(Client)/DataObject/DataStructure/EHDataStructure.cs
@@ -0,0 +1,21 @@
+﻿using CloudyHoa_Client_.General;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace CloudyHoa_Client_.DataObject.DataStructure
+{
+    public class EHDataStructure : IDataStructure
+    {
+        public EHDataStructure() { }
+        public DataTable ServicesTable { get; set; }
+
+        public int CurrentService { get; set; }
+
+        public DateTime CurrentDate { get; set; }   
+
+        public DataTable EnteringTable { get; set; }
+
+        public DataTable TemporaryEnteringTable { get; set; } = new DataTable();
+    }
+}
