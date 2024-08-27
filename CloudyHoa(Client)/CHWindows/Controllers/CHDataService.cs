e21def87eaf5f3d8337ced8849062bc356755289
Богдан Т <bogdantribusyan2016@gmail.com>
2024-08-27 10:55:00 +0300

    Клиент для сервиса получения истории

---
+++ CloudyHoa(Client)/CHWindows/Controllers/CHDataService.cs
@@ -0,0 +1,48 @@
+﻿using CloudyHoa_Client_.CHService;
+using CloudyHoa_Client_.EHService;
+using CloudyHoa_Client_.General;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudyHoa_Client_.CHWindows.Controllers
+{
+    public class CHDataService : IDataService
+    {
+        CHServiceClient _chServiceClient;
+        public CHDataService()
+        {
+            _chServiceClient = new CHServiceClient();
+        }
+
+        public async Task<DataTable> GetCalculatingHistoryOD(int objectId, DateTime period)
+        {
+            DataSet dataSet = null;
+            dataSet = await _chServiceClient.GetCalculationHistoryFromObjectAsync(objectId, period);
+            if (dataSet != null)
+            {
+                return dataSet.Tables[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public async Task<DataTable> GetCalculatingHistoryPA(int paId, DateTime period)
+        {
+            DataSet dataSet = null;
+            dataSet = await _chServiceClient.GetCalculationHistoryFromPAAsync(paId, period);
+            if (dataSet != null)
+            {
+                return dataSet.Tables[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
