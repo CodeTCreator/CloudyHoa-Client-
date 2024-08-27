cbc6880841f0bfca181570dd0926ff81264105bd
Богдан Т <bogdantribusyan2016@gmail.com>
2024-08-27 10:55:58 +0300

    Структура данных для истории расчетов

---
+++ CloudyHoa(Client)/CHWindows/Controllers/CHDataStructure.cs
@@ -0,0 +1,21 @@
+﻿using CloudyHoa_Client_.DataObject;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace CloudyHoa_Client_.CHWindows.Controllers
+{
+    public class CHDataStructure :IDataStructure
+    {
+        public CHDataStructure() { }
+        public DataTable ServicesTable { get; set; }
+
+        public int CurrentService { get; set; }
+
+        public DateTime CurrentDate { get; set; }
+
+        public DataTable CalculatingTable { get; set; }
+
+        public DataTable TemporaryCalculatingTable { get; set; } = new DataTable();
+    }
+}
